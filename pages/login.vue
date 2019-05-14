689612ae50e5e2920ca1dcb2dbcf3551039cbfa6
mengen <1647072238@qq.com>
2019-05-14 19:53:14 +0800

    login

---
+++ pages/login.vue
@@ -0,0 +1,82 @@
+<template>
+  <div>
+      <div class="top">
+        <div class="logoWrapper">
+          <a href="/" class="login-logo"></a>
+        </div>
+      </div>
+      <div class="container">
+        <img src="http://s0.meituan.net/bs/file/?f=fe-sso-fs:build/page/static/banner/www.jpg" alt="">
+        <div class="right">
+          <el-input type="text" placeholder="邮箱"></el-input>
+          <el-input type="password" placeholder="密码"></el-input>
+          <div class="btnWrapper">
+            <el-button type="primary">登录</el-button>
+          </div>
+          <p>还没有账号? <nuxt-link to="/register">免费注册</nuxt-link></p>
+        </div>
+      </div>
+  </div>
+</template>
+
+<script>
+export default {
+  layout:'blonk'
+}
+</script>
+
+<style lang="scss" scoped>
+  .top{
+    width: 55%;
+    margin: 0 auto;
+    height: 96px;
+    .logoWrapper{
+       padding: 30px 0px;
+    }
+    .login-logo{
+      background-image: url(https://s0.meituan.net/bs/file/?f=fe-sso-fs:build/assets/sp-normal.2ee5c09.png);
+      width: 82px;
+      height: 54px;
+      background-position: -188px -808px;
+      display: block;
+      padding: 10px 0px;
+      float: left;
+    } 
+  }
+  .container{
+    width: 55%;
+    margin: 0 auto;
+    margin-top: 50px;
+    overflow: hidden;
+    img{
+      width: 480px;
+      height: 370px;
+      float: left;
+    }
+    .right{
+      float: right;
+      margin-right: 30px;
+      p{
+        margin-top: 8px;
+        color: #666;
+      }
+    }
+    .right /deep/ .el-input{
+        width: 72%;
+        margin-bottom: 20px;
+        border-radius: 2px;
+    }
+    .btnWrapper{
+      width: 72%;
+    }
+    .btnWrapper /deep/ .el-button{
+      color: #fff;
+      background-color: #2eb7aa;
+      border-color: #008177;
+      width: 100%;
+      border-radius: 2px;
+    }
+  }
+</style>
+
+
---
+++ pages/register.vue
@@ -0,0 +1,181 @@
+<template>
+  <div>
+    <div class="m-register">
+      <div class="register-container">
+        <div class="top">
+          <a href="/" class="register-logo"></a>
+          <div class="desc">
+            <span>已有美团账号?</span>
+            <a href="/login" class="login">
+              <el-button type="primary" size="mini">登录</el-button>
+            </a>
+          </div>
+        </div>
+      </div>
+    </div>
+    <div class="form-content">
+      <div class="myform">
+        <el-form :model="ruleForm" :rules="rules" ref="ruleForm" label-width="100px" class="demo-ruleForm">
+            <el-form-item label="昵称" prop="name">
+              <el-input v-model="ruleForm.name"></el-input>
+            </el-form-item>
+            <el-form-item label="邮箱" prop="email">
+             <el-input v-model="ruleForm.email"></el-input>
+             <el-button size="mini" round @click="sendMsg">发送验证码</el-button>
+            <span class="status">{{ statusMsg }}</span>
+            </el-form-item>
+            <el-form-item label="验证码" prop="code">
+              <el-input v-model="ruleForm.code" maxlength="4"/>
+            </el-form-item>
+            <el-form-item label="密码" prop="pwd">
+               <el-input type="password" v-model="ruleForm.pwd"></el-input>
+            </el-form-item>
+            <el-form-item label="确认密码" prop="cpwd" >
+               <el-input type="password" v-model="ruleForm.cpwd"></el-input>
+            </el-form-item>
+            <el-form-item class="formbtn">
+            <el-button type="primary" @click="register">同意以下协议并注册</el-button>
+              <div class="error">{{ error }}</div>
+            </el-form-item>
+              <el-form-item>
+              <a class="f1" href="http://www.meituan.com/about/terms" target="_blank">《美团网用户协议》</a>
+            </el-form-item>
+          </el-form>
+      </div>
+    </div>
+  </div>
+
+</template>
+
+<script>
+export default {
+  layout: 'blonk',
+  data () {
+    return {
+      error:'',
+      statusMsg:'',
+      ruleForm: {
+        name: '',
+        email: '',
+        pwd: '',
+        cpwd: '',
+        code: ''
+      },
+      rules:{
+        name: [{
+          required: true,
+          type: 'string',
+          message: '请输入昵称',
+          trigger: 'blur'
+        }],
+        email: [{
+          required: true,
+          type: 'email',
+          message: '请输入邮箱',
+          triger: 'blur'
+        }],
+        pwd: [{
+          required: true,
+          message: '创建密码',
+          trigger: 'blur'
+        }],
+        code: [{
+          required: true,
+          message: '输入验证码',
+          trigger: 'blur'
+        }],
+        cpwd: [{
+          required: true,
+          message: '确认密码',
+          validator: (rule,value,cb)=>{
+            if(value === ''){
+              cb(new Error('请再次确认密码'))
+            }else if( value !== this.ruleForm.pwd){
+              cb(new Error('两次密码不一致'))
+            }else{
+              cb()
+            }
+          }
+        }]
+      }
+    }
+  },
+  methods: {
+    sendMsg () {
+
+    },
+    register () {
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .m-register{
+    width: 100%;
+    min-width: 980px;
+    border-bottom: 2px solid #2DB8AA;
+    height: 55px;
+    .register-container{
+      width: 55%;
+      margin: 0 auto;
+      .top{
+        height: 55px;
+        overflow: hidden;
+      }
+      .register-logo{
+        background-image: url(https://s0.meituan.net/bs/file/?f=fe-sso-fs:build/assets/sp-normal.2ee5c09.png);
+        width: 54px;
+        height: 36px;
+        background-position: -669px -739px;
+        display: block;
+        padding: 10px 0px;
+        float: left;
+      }
+      .desc{
+        float: right;
+        line-height: 36px;
+        padding-top: 10px;
+        box-sizing: border-box;
+        .login /deep/ .el-button{
+          background: #2DB8AA;
+          border-color: #2DB8AA;
+          margin-left: 5px;
+        }
+      }
+    }
+  }
+  .form-content{
+    width: 55%;
+    margin: 0 auto;
+    .myform{
+      width: 360px;
+      margin-top: 30px;
+    }
+    .myform /deep/ .el-input__inner{
+      height: 36px;
+      line-height: 36px;
+      border-radius: 2px;
+      
+    }
+    .myform /deep/ .is-round{
+      padding: 3px 40px;
+      border-radius: 0px;
+      color: #333;
+      background-color: #dedede;
+      border: 1px solid #e3e3e3;
+      border-bottom: 1px solid #aaa;
+      background-image: url(data:image/svg+xml;base64,PD94bWwgdmVyc2lvbj0iMS4wIiBlbmNvZGluZz0idXRmLTgiP…dpZHRoPSIxMDAlIiBoZWlnaHQ9IjEwMCUiIGZpbGw9InVybCgjZ3JhZCkiIC8+PC9zdmc+IA==);
+      background-size: 100%; 
+      background-image: linear-gradient(to bottom,#f7f7f7,#dedede);
+    }
+    .formbtn /deep/ .el-button{
+      background: #2DB8AA;
+      border-color: #2DB8AA;
+    }
+    
+  }
+  
+</style>
+
+

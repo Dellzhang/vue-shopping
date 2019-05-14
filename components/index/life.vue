0323df89165d1955fe6986788b226a6c512cfc97
mengen <1647072238@qq.com>
2019-05-14 20:16:29 +0800

    b

---
+++ components/index/life.vue
@@ -0,0 +1,94 @@
+<template>
+  <el-row class="m-left">
+    <el-col :span="7" class="jiudian">
+      <img src="http://p1.meituan.net/codeman/8cce56c467a17e04f3094d1e455462a0132772.png" alt="">
+    </el-col>
+    <el-col :span="7" class="jiudian two">
+      <img src="http://p1.meituan.net/codeman/16442c19da1f1c4544f794e29d99c92051716.jpg" alt="">
+    </el-col>
+    <el-col :span="4" class="shop">
+      <img src="http://p1.meituan.net/codeman/5b21cddb4bb1cbc3a9c3bce0f726c75940469.jpg" alt="">
+    </el-col>
+    <el-col :span="4">
+      <div class="download-app">
+        <div class="qrcode-box">
+            <img src="http://s1.meituan.net/bs/fe-web-meituan/60ac9a0/img/download-qr.png" alt="">
+        </div>
+        <p class="desc-top">美团APP手机版</p>
+        <p class="desc-bottom">
+          <span class="desc-bottom-left">1元起</span>
+          <span class="desc-bottom-right">吃喝玩乐</span>
+        </p>
+      </div>
+    </el-col>
+  </el-row>
+</template>
+
+<script>
+export default {
+    
+}
+</script>
+
+<style lang="scss" scoped>
+  .m-left{
+    margin-top: 6px;
+    img{
+      cursor: pointer;
+    }
+    .jiudian{
+      img{
+        width: 270px;
+        height: 165px;
+      }
+      &.two{
+        margin-right: 10px;
+        img{
+          width: 275px;
+        }
+      }
+    }
+    .shop{
+      img{
+        width: 150px;
+        height: 165px;
+      }
+    }
+    .download-app{
+      width:230px;
+      height: 167px;
+      box-sizing: border-box;
+      border: 1px solid #e5e5e5;
+      background: #fff;
+      .qrcode-box{
+        margin-top: 10px;
+        text-align: center;
+        img{
+          width: 95px;
+          height: 95px;
+        }
+      }
+      .desc-top{
+        font-size: 16px;
+        color: #222;
+        font-weight: 500;
+        text-align: center;
+      }
+      .desc-bottom{
+         font-weight: 500;
+         text-align: center;
+         font-size: 12px;
+        .desc-bottom-left{
+          color: #EC5330;
+          margin-right: 5px;
+        }
+        .desc-bottom-right{
+          color: #3f3f3f;
+        }
+      }
+
+    }
+  }
+</style>
+
+
---
+++ components/index/slide.vue
@@ -0,0 +1,126 @@
+<template>
+  <div class="m-slide">
+    <el-row>
+      <el-col :span="14">
+        <div class="block">
+          <el-carousel height="240px;width:550px">
+            <el-carousel-item v-for="(item,index) of imgList" :key="index">
+             <img :src="item" alt="" class="slide-img">
+            </el-carousel-item>
+          </el-carousel>
+        </div>
+      </el-col>
+      <el-col :span="4" class="ktv">
+        <div class="ktv-img-wrapper">
+          <img src="http://p0.meituan.net/codeman/e473bb428f070321269b23370ff02ba956209.jpg" alt="" class="ktv-img">
+        </div>
+      </el-col>
+      <el-col :span="4">
+        <div class="login-cotainer">
+          <div class="default">
+            <div class="userlogo">
+              <img src="http://s0.meituan.net/bs/fe-web-meituan/2d05c2b/img/avatar.jpg" alt="">
+            </div>
+            <p>Hi! 你好</p>
+            <nuxt-link to="/login" class="login">登录</nuxt-link>
+            <nuxt-link to="/register" class="login">注册</nuxt-link>
+          </div>
+        </div>
+      </el-col>
+    </el-row>
+    <index-life></index-life>
+  </div>
+</template>
+
+<script>
+import IndexLife from './life'
+export default {
+  components: {
+    IndexLife
+  },
+  data () {
+    return {
+      imgList:['http://p1.meituan.net/codeman/826a5ed09dab49af658c34624d75491861404.jpg','http://p0.meituan.net/codeman/33ff80dc00f832d697f3e20fc030799560495.jpg','http://p1.meituan.net/codeman/826a5ed09dab49af658c34624d75491861404.jpg']
+    }
+  }
+}
+</script>
+
+<style lang="scss" scoped>
+  .m-slide{
+    margin: 10px 0px;
+    .block{
+      overflow: hidden;
+    }
+    .block /deep/ .el-carousel{
+      width: 550px;
+      height: 240px;
+      overflow: hidden;
+    }
+    .block /deep/ .slide-img{
+      width: 550px;
+      height: 240px;
+      cursor: pointer;
+    }
+    .block /deep/ .el-carousel__arrow{
+      background: #585756;
+    }
+    .ktv{
+      margin-right: 10px;
+      .ktv-img-wrapper{
+        display: inline-block;
+        margin: 0px 10px;
+        .ktv-img{
+          width: 150px;
+          height: 240px;
+          cursor: pointer;
+        }   
+      }
+    }
+    .login-cotainer{
+      width: 230px;
+      height: 240px;
+      box-sizing: border-box;
+      border: 1px solid #e5e5e5;
+      background: #fff;
+      .default{
+        padding-top: 30px;
+        .userlogo{
+          text-align: center;
+          img{
+            width: 47px;
+            height: 47px;
+            border-radius: 50%;
+            border: 4px solid #E5E5E5;
+          }
+        }
+        p{
+          text-align: center;
+          color: #222;
+          font-size: 16px;
+          font-weight: 500;
+        }
+        .login{
+          width: 118px;
+          color: #333;
+          font-size: 14px;
+          background: #fff;
+          border: 1px solid #e5e5e5;
+          display: block;
+          text-align: center;
+          margin: 10px auto 15px;
+          line-height: 38px;
+          border-radius: 40px;
+          transition: background-color 0.5s;
+          &:hover{
+            background: #F6F6F6;
+
+          }
+        }
+      }
+    }
+    
+  }
+</style>
+
+
